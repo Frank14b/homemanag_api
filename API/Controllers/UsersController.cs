308098f89703d504ba1bdd6a492c1ee4720994a0
FrankFontcha <frank.fontcha@vaimanagement.co>
2023-02-18 15:38:26 +0400

    initial commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] //api/users
+    public class UsersController
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            this._context = context;
+        }
+        
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = this._context.Users.ToList();
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUsers(int id)
+        {
+            return await this._context.Users.FindAsync(id);
+        }
+    }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,24 @@
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public int Id {get; set;}
+
+        public string UserName {get; set;}
+
+        public string FirstName {get; set;}
+
+        public string LastName {get; set;}
+
+        public int Status {get; set;}
+
+        public string Email {get; set;}
+
+        public string Password {get; set;}
+
+        public DateTime CreatedAt {get; set;}
+
+        public DateTime UpdatedAt {get; set;}
+    }
+}
